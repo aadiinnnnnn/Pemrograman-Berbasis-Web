d396f47368d87276655792bc79043711ae9a87e8
aadiinnnnnn <adinoaza@gmail.com>
2025-03-26 20:52:22 +0700

    First commit

---
+++ pertemuan 3/script.js
@@ -0,0 +1,141 @@
+// function handleClick() {
+// alert("aku mau di pencet");
+// }
+
+// let name;
+// name = "Adin ganteng";
+// console.log(name);
+
+// let kota;
+// kota = "bandung";
+// console.log(kota);
+// let hewan;
+// hewan = "macan";
+// console.log(hewan);
+// let prodi = "SI";
+// console.log(prodi);
+
+// const phi = 100;
+// const greet = "adin";
+
+// console.log("nilai adin adalah " + phi);
+// console.log("good morning " + greet);
+
+// let a = 5; //int
+// let b = "10"; //string
+
+// array
+// let fakultas = ["SI", "TI", "HI", "IKOM"];
+// console.log(fakultas[0]);
+// console.log(fakultas[1]);
+// console.log(fakultas[2]);
+
+// ngubah data array
+// let fakultas = ["SI", "TI", "HI", "IKOM"];
+// fakultas[0] = "FAPERTA";
+// console.log(fakultas[0]);
+
+// array 2 dimensi
+// let coordinate = [[0], [1], [2]];
+
+// // op artimatika
+// let hasil = 9 + 10;
+// console.log(hasil);
+
+// op perbandingan
+// let result = 10 == 2;
+// let results = 10 === "2"; // untuk perbandingan dengan character
+// console.log(result);
+
+// // op Logika
+// let bool;
+// bool = true && false; // dan
+// console.log(bool);
+// bool = false || true; // or
+// console.log(bool);
+// bool = !false; // kebalikan
+// console.log(bool);
+// bool = true || true || false;
+// console.log(bool);
+// bool = (true && false) || true;
+// console.log(bool);
+
+// op spread
+// let male = ["adin", "nayaka", "tito"];
+// console.log(male);
+// let female = ["adin", "nayaka", "titos"];
+// console.log(female);
+// let mahasiswa = [...female, ...male]; // syntax spread (...)
+// console.log(mahasiswa);
+
+// op percabangan atau if else
+// let gender = "L";
+// if (gender === "S") {
+//   alert("Aku lanang mas");
+// } else {
+//   alert("jk gak di ketahui");
+// }
+
+// if else if
+// let username, password;
+// username = "admin";
+// password = "querty";
+// if (username === "admin" && password === "querty") {
+//   alert("Gokil masuk");
+// } else if (username === "admin" && password !== "querty") {
+//   alert("salah lo"); // password salah
+// } else if (username !== "admin" && password === "querty") {
+//   alert("username lo salah bang"); // username salah
+// } else {
+//   alert("SALAH USER SAMA PW LO");
+// } // salah 22nya
+
+// // swtich break
+// let grade = "A";
+// switch (grade) {
+//   case "A":
+//     console.log("Nilai 100, GG");
+//     break;
+//   case "B":
+//     console.log("80 bang");
+//     break;
+//   case "C":
+//     console.log("70 bang");
+//     break;
+//   default:
+//     console.log("Belajar pls");
+//     break;
+// }
+
+// op conditional
+// let gender = "L";
+// if (gender === "S") {
+//   alert("Aku lanang mas");
+// } else {
+//   alert("jk gak di ketahui");
+// }
+// ternary operator
+// syntax dasar : let variable = kondisi ? true : false
+// let hasil = gender === "L" ? "Aku lanang" : "Jk gak tau";
+// console.log(hasil);
+
+// for
+// for (let i = 0; i < 10; i++) {
+//   console.log("hari ke " + i);
+// }
+
+// while
+// let i = 50;
+// while (i > 0) {
+//   console.log(i + " * 5 =" + i * 5);
+//   i = i - 5;
+// }
+
+// do while
+// let i = 1;
+// do {
+//   console.log(i + " kuadrat = " + i * i);
+//   i = i + i;
+// } while (i <= 0);
+
+// for of untuk mengambil data di dalam array yg bertambah
